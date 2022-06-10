10d345f17fdab0d973edefb45d0eacd8666cff4d
Goodness Urama <goodyboy301@gmail.com>
2022-06-10 13:04:28 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(process.env.PORT || "3000", console.log("listening..."));
 
